ba91c1109902fff03e552a8460c8f1baf509ad3c
baltasar <baltasarq@gmail.com>
2023-01-03 20:42:19 +0100

    Bumped version

--- app/src/main/java/com/devbaltasarq/nadamillas/core/AppInfo.java
+++ app/src/main/java/com/devbaltasarq/nadamillas/core/AppInfo.java
@@ -7,10 +7,10 @@ package com.devbaltasarq.nadamillas.core;
 public class AppInfo {
     public static final String AUTHOR = "dev::baltasarq";
     public static final String EMAIL = "baltasarq@gmail.com";
-    public static final String VERSION = "1.2.2";
-    public static final String SERIAL = "20221015";
+    public static final String VERSION = "1.2.3";
+    public static final String SERIAL = "20230104";
     public static final String LICENSE = "MIT license";
-    public static final String COPYRIGHT = "(c) 2019/22";
+    public static final String COPYRIGHT = "(c) 2019/23";
 
     public static String getAuthoringMessage()
     {
