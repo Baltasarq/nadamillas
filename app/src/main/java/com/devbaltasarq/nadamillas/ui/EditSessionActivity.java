1bac88cd07522796e68f6e45be77028dbeae3fe4
baltasarq <baltasarq@gmail.com>
2023-11-27 12:20:23 +0100

    Adds laps calculation to session editing

--- app/src/main/java/com/devbaltasarq/nadamillas/ui/EditSessionActivity.java
+++ app/src/main/java/com/devbaltasarq/nadamillas/ui/EditSessionActivity.java
@@ -1,20 +1,29 @@
 // NadaMillas (c) 2019 Baltasar MIT License <baltasarq@gmail.com>
 
+
 package com.devbaltasarq.nadamillas.ui;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.devbaltasarq.nadamillas.core.Settings;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.devbaltasarq.nadamillas.R;
@@ -26,6 +35,7 @@ import com.devbaltasarq.nadamillas.core.storage.SessionStorage;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+
 
 public class EditSessionActivity extends BaseActivity {
     private static final String LOG_TAG = EditSessionActivity.class.getSimpleName();
@@ -44,6 +54,7 @@ public class EditSessionActivity extends BaseActivity {
         final TextView LBL_TITLE = this.findViewById( R.id.lblTitle );
         boolean isEdit = false;
 
+        this.blockListeners = true;
         this.date = Util.getDate().getTime();
         this.duration = new Duration( 0 );
 
@@ -73,6 +84,7 @@ public class EditSessionActivity extends BaseActivity {
         }
 
         // Update view
+        final LinearLayout LY_POOL_LAPS = this.findViewById( R.id.lyPoolLaps );
         final ImageButton BT_BACK = this.findViewById( R.id.btCloseEditSession );
         final FloatingActionButton FB_SAVE = this.findViewById( R.id.fbSaveSession );
         final EditText ED_DATE = this.findViewById( R.id.edDate );
@@ -83,17 +95,69 @@ public class EditSessionActivity extends BaseActivity {
         final ImageButton BT_DATE = this.findViewById( R.id.btDate );
         final RadioGroup GRD_WATER_TYPES = this.findViewById( R.id.grdWaters );
         final RadioButton RBT_POOL = this.findViewById( R.id.rbtPool );
-        final RadioButton RBT_OPEN = this.findViewById( R.id.rbtOpen );
+        final RadioButton RBT_OWS = this.findViewById( R.id.rbtOpen );
         final ImageButton BT_SAVE = this.findViewById( R.id.btSaveSession );
-        final ImageButton BT_SHARE = this.findViewById( R.id.btShareSessionEdited );
         final ImageButton BT_SCRSHOT = this.findViewById( R.id.btTakeScrshotForEditSession );
+        final EditText ED_LAPS = this.findViewById( R.id.edLaps );
+        final Spinner CB_POOL_LENGTH = this.findViewById( R.id.cbPoolLength );
+        final TextView LBL_LENGTH1 = this.findViewById( R.id.lblLength1 );
+        final TextView LBL_LENGTH2 = this.findViewById( R.id.lblLength2 );
 
+        // Prepares the label for distance
+        if ( settings.getDistanceUnits() == Settings.DistanceUnits.mi ) {
+            LBL_LENGTH1.setText( R.string.label_yard );
+            LBL_LENGTH2.setText( R.string.label_yard );
+        }
+
+        // Prepares pool length & laps spinner
+        final ArrayAdapter<String> POOL_LENGTH_ADAPTER = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                new String[]{ "25", "50", "100" });
+
+        CB_POOL_LENGTH.setAdapter( POOL_LENGTH_ADAPTER );
+        CB_POOL_LENGTH.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                EditSessionActivity.this.calculatePoolLaps();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+            }
+        });
+
+        // Prepares the laps editor
+        ED_LAPS.setText( "0" );
+        ED_LAPS.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                EditSessionActivity.this.calculateDistanceByPoolLength();
+            }
+        });
+
+        // Prepare remaining widgets
         ED_DATE.setText( Util.getShortDate( this.date, null ) );
 
         if ( this.atPool ) {
             GRD_WATER_TYPES.check( RBT_POOL.getId() );
         } else {
-            GRD_WATER_TYPES.check( RBT_OPEN.getId() );
+            GRD_WATER_TYPES.check( RBT_OWS.getId() );
         }
 
         if ( this.distance > 0 ) {
@@ -107,21 +171,25 @@ public class EditSessionActivity extends BaseActivity {
         }
 
         RBT_POOL.setCompoundDrawablesWithIntrinsicBounds( 0, 0, R.drawable.ic_pool, 0 );
-        RBT_OPEN.setCompoundDrawablesWithIntrinsicBounds( 0, 0, R.drawable.ic_sea, 0 );
+        RBT_POOL.setOnCheckedChangeListener( (bt, isChecked) -> {
+            if ( isChecked ) {
+                LY_POOL_LAPS.setVisibility( View.VISIBLE );
+            }
+        });
+        RBT_OWS.setCompoundDrawablesWithIntrinsicBounds( 0, 0, R.drawable.ic_sea, 0 );
+        RBT_OWS.setOnCheckedChangeListener( (bt, isChecked) -> {
+            if ( isChecked ) {
+                LY_POOL_LAPS.setVisibility( View.GONE );
+            }
+        });
 
         // Set listeners
         BT_BACK.setOnClickListener( v -> EditSessionActivity.this.finish() );
 
-        BT_SHARE.setOnClickListener( v -> {
-            final EditSessionActivity SELF = EditSessionActivity.this;
-
-            SELF.share( LOG_TAG, SELF.takeScreenshot( LOG_TAG ) );
-        });
-
         BT_SCRSHOT.setOnClickListener( v -> {
             final EditSessionActivity SELF = EditSessionActivity.this;
 
-            SELF.saveScreenShotToDownloads( LOG_TAG, SELF.takeScreenshot( LOG_TAG ) );
+            SELF.shareScreenShot( LOG_TAG, SELF.takeScreenshot( LOG_TAG ) );
         });
 
         BT_SAVE.setOnClickListener( v -> EditSessionActivity.this.save() );
@@ -148,7 +216,7 @@ public class EditSessionActivity extends BaseActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 EditSessionActivity.this.storeDistance( s );
-                EditSessionActivity.this.calculateMeanSpeed();
+                EditSessionActivity.this.recalculate();
             }
         });
 
@@ -166,7 +234,7 @@ public class EditSessionActivity extends BaseActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 EditSessionActivity.this.storeDuration();
-                EditSessionActivity.this.calculateMeanSpeed();
+                EditSessionActivity.this.recalculate();
             }
         });
 
@@ -184,7 +252,7 @@ public class EditSessionActivity extends BaseActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 EditSessionActivity.this.storeDuration();
-                EditSessionActivity.this.calculateMeanSpeed();
+                EditSessionActivity.this.recalculate();
             }
         });
 
@@ -202,9 +270,11 @@ public class EditSessionActivity extends BaseActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 EditSessionActivity.this.storeDuration();
-                EditSessionActivity.this.calculateMeanSpeed();
+                EditSessionActivity.this.recalculate();
             }
         });
+
+        this.blockListeners = false;
 
         // Set focus
         ED_DISTANCE.requestFocus();
@@ -215,7 +285,7 @@ public class EditSessionActivity extends BaseActivity {
     {
         super.onResume();
 
-        this.calculateMeanSpeed();
+        this.recalculate();
     }
 
     /** Launch a date picker dialog. */
@@ -309,6 +379,73 @@ public class EditSessionActivity extends BaseActivity {
         this.duration = new Duration( hours, minutes, seconds );
     }
 
+    /** Recalculate */
+    private void recalculate()
+    {
+        this.calculatePoolLaps();
+        this.calculateMeanSpeed();
+    }
+
+    /** Gets the value of the pool length spinner. */
+    private int getPoolLength()
+    {
+        final Spinner CB_POOL_LENGTH = this.findViewById( R.id.cbPoolLength );
+        final String POOL_LENGTH = (String) CB_POOL_LENGTH.getSelectedItem();
+        int toret = 25;
+
+        if ( POOL_LENGTH != null ) {
+            try {
+                toret = Integer.parseInt( POOL_LENGTH );
+            } catch(NumberFormatException exc) {
+                Log.e( LOG_TAG, "Pool length incorrect: " + POOL_LENGTH );
+            }
+        }
+
+        return toret;
+    }
+
+    /** Gets the value of the number of laps editor. */
+    private int getNumLaps()
+    {
+        final EditText ED_LAPS = this.findViewById( R.id.edLaps );
+        final String STR_LAPS = ED_LAPS.getText().toString();
+        int toret = 0;
+
+        try {
+            toret = Integer.parseInt( STR_LAPS );
+        } catch(NumberFormatException exc) {
+            Log.e( LOG_TAG, "Num laps incorrect: " + STR_LAPS );
+        }
+
+        return toret;
+    }
+
+    /** Calculates the number of pool laps needed for the distance. */
+    private void calculatePoolLaps()
+    {
+        if ( !this.blockListeners ) {
+            final Session FAKE_SESSION = new Session( this.date, this.distance, this.duration, this.atPool );
+            final EditText ED_POOL_LAPS = this.findViewById( R.id.edLaps );
+            int poolLength = this.getPoolLength();
+            int numLaps = 0;
+
+            if ( poolLength > 0 ) {
+                int modLaps = FAKE_SESSION.getDistance() % poolLength;
+                numLaps = FAKE_SESSION.getDistance() / poolLength;
+
+                if ( modLaps != 0 ) {
+                    ++numLaps;
+                }
+            }
+
+            this.blockListeners = true;
+            ED_POOL_LAPS.setText( Integer.toString( numLaps ) );
+            this.blockListeners = false;
+        }
+
+        return;
+    }
+
     /** Calculates and shows the mean speed as data is entered. */
     private void calculateMeanSpeed()
     {
@@ -321,10 +458,26 @@ public class EditSessionActivity extends BaseActivity {
         ) );
     }
 
+    /** Calculates the distance given the laps. */
+    private void calculateDistanceByPoolLength()
+    {
+        if ( !this.blockListeners ) {
+            final EditText ED_DISTANCE = this.findViewById( R.id.edDistance );
+            int laps = this.getNumLaps();
+            int poolLength = this.getPoolLength();
+
+            this.blockListeners = true;
+            ED_DISTANCE.setText( "" + ( laps * poolLength ) );
+            this.blockListeners = false;
+        }
+
+        return;
+    }
+
     @Override
     protected void update()
     {
-        this.calculateMeanSpeed();
+        this.recalculate();
     }
 
     /** Saves the data and returns. */
@@ -345,5 +498,6 @@ public class EditSessionActivity extends BaseActivity {
     private Date date;
     private int distance;
     private Duration duration;
+    private boolean blockListeners;
     private boolean atPool;
 }
