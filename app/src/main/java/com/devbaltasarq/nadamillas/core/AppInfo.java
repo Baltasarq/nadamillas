6fbfcfcba3c24338811230cb14f2ebc9d027a419
baltasarq <baltasarq@gmail.com>
2024-09-30 10:46:51 +0200

    Bumps version

--- app/src/main/java/com/devbaltasarq/nadamillas/core/AppInfo.java
+++ app/src/main/java/com/devbaltasarq/nadamillas/core/AppInfo.java
@@ -1,4 +1,4 @@
-// NadaMillas (c) 2019-2024-2024 Baltasar MIT License <baltasarq@gmail.com>
+// NadaMillas (c) 2019-2023-2024 Baltasar MIT License <baltasarq@gmail.com>
 
 
 package com.devbaltasarq.nadamillas.core;
@@ -7,8 +7,8 @@ package com.devbaltasarq.nadamillas.core;
 public class AppInfo {
     public static final String AUTHOR = "dev::baltasarq";
     public static final String EMAIL = "baltasarq@gmail.com";
-    public static final String VERSION = "1.3.2";
-    public static final String SERIAL = "20240114";
+    public static final String VERSION = "1.3.5";
+    public static final String SERIAL = "20240929";
     public static final String LICENSE = "MIT license";
     public static final String COPYRIGHT = "(c) 2019-2024";
 
