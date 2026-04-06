8534be9daccba3808242137e9e024ce55b577b79
baltasar <baltasarq@gmail.com>
2026-04-06 14:57:33 +0200

    Bumped version

--- app/src/main/java/com/devbaltasarq/nadamillas/core/AppInfo.java
+++ app/src/main/java/com/devbaltasarq/nadamillas/core/AppInfo.java
@@ -10,7 +10,7 @@ public class AppInfo {
     public static final String VERSION = "1.4.0";
     public static final String SERIAL = "20260404";
     public static final String LICENSE = "MIT license";
-    public static final String COPYRIGHT = "(c) 2019-2025";
+    public static final String COPYRIGHT = "(c) 2019-2026";
 
     public static String getAuthoringMessage()
     {
